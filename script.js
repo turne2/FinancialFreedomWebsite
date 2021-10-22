bc846468081901d26986881e205eb6e4d09e47f4
Elisa Turner <88210013+ElisaTurner@users.noreply.github.com>
2021-10-21 21:18:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-not updated yet 
+function calcSalary() {
+ var wage = parseFloat(document.getElementById('txt_wage').value);
+ 
+var hours = parseFloat(document.getElementById('txt_hours').value);
+  
+var calculate = wage * hours * 52.0;
+ 
+document.getElementById('results').innerHTML = calculate;
+}
